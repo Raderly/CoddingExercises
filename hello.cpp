b52d7bc20e53a8b07f6b24abb54ef1ca03736b9c
Raderly <raderly.ch@gmail.com>
2022-10-03 00:57:13 -0500

    Check first changes

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,6 @@ int main(int argc, char const *argv[])
 {
     std::cout<<"Hola mundo y github!\n";
     std::cout<<"Example Repo"<<std::endl;
+    std::cout<<"checking changes"<<std::endl
     return 0;
 }
